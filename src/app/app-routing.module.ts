a71b131586cea99e004813488b74ed3c4817460e
SethOfCarso <cardona.erick@hotmail.com>
2020-05-08 11:35:45 -0500

    Changed routing to main post component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post/post-main/post-list/post-list.component';
+import { PostMainComponent } from './post/post-main/post-main.component';
+import { PostDetailComponent } from './post/post-main/post-detail/post-detail.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: PostListComponent},
-  {path: 'post', component: PostListComponent},
-  {path: '', component: PostListComponent},
+  {path: 'home', component: PostMainComponent, children:[
+    {path: '', component: PostListComponent},
+      {path: 'post-list', component: PostListComponent},
+      {path: 'post-detail', component: PostDetailComponent}
+  ]},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   {path: '**', component: NotFoundComponent}
 ];
 

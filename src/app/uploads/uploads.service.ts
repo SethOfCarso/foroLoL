89c5541e43e52158dac086d50317b91fd007f0e6
Elias <eGasparArellano@gmail.com>
2020-05-10 17:47:11 -0500

    Restructuring folders

--- src/app/uploads/uploads.service.ts
+++ src/app/uploads/uploads.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from './auth.service';
+import { AuthService } from '../auth/auth.service';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 // import { environment } from 'src/environments/environment.prod';
